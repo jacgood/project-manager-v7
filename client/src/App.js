0f333a9f6d5582d9cfc45a15fa857aff119468d4
Jacob Good <jgood@good-it-solutions.com>
2020-06-14 14:53:45 -0400

    made copyright a landing page component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Routes from './components/routing/Routes'
-import Login from './components/Login/Login'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Routes from "./components/routing/Routes";
+import Login from "./components/Login/Login";
+import Copyright from "./components/layout/Copyright";
+import Box from "@material-ui/core/Box";
 
 function App() {
   return (
@@ -11,10 +13,12 @@ function App() {
           <Route exact path="/" component={Login} />
           <Route component={Routes} />
         </Switch>
-        {/* <Projects /> */}
+        <Box mt={8}>
+          <Copyright />
+        </Box>
       </>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
